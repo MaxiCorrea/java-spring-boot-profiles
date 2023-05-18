b78971609c33c4485bc0857c6166a97386a43ed9
Maximiliano Correa <jmaxicorrea@gmail.com>
2023-05-17 21:11:44 -0300

    Update Profiles -> Link Runtime Profile & Build Profile

--- src/main/java/com/maxicorrea/javaspringprofiles/JavaSpringProfilesApplication.java
+++ src/main/java/com/maxicorrea/javaspringprofiles/JavaSpringProfilesApplication.java
@@ -1,6 +1,7 @@
 package com.maxicorrea.javaspringprofiles;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,9 @@ import com.maxicorrea.javaspringprofiles.config.DataSourceConfig;
 
 @SpringBootApplication
 public class JavaSpringProfilesApplication {
+  
+  @Value("${spring.profiles.active}")
+  private String profile;
   
   @Autowired
   private DataSourceConfig dataSourceConfig;
@@ -18,6 +22,7 @@ public class JavaSpringProfilesApplication {
   
   @Bean
   public void setupDataSource() {
+    System.out.println(profile);
     dataSourceConfig.setup();
   }
 }
